7f77f363192b4ef8063b799bb3a2fae42d7d4898
Gastón <Gastonnietoarte@gmail.com>
2023-07-19 22:59:30 -0300

    Gaston (#17)
    * feat: api/routes - añadido manejadores de rutas
    
    * feat: src/handlers - añado manejadores de rutas admin y client

---
+++ api/src/handlers/adminRoute.js
@@ -0,0 +1,69 @@
+const { Router } = require("express");
+const router = Router();
+
+
+// GET:
+router.get("/sales", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+
+router.get("/orders", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+
+router.get("/clients", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+
+//POST
+router.post("/create/product", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+
+//PUT
+router.put("/update/product", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+
+//DELETE
+router.delete("/delete/product", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+
+module.exports = router;
