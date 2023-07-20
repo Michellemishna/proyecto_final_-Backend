0b6055718f157e1eab2237434808d90310d344e8
Gastón <Gastonnietoarte@gmail.com>
2023-07-19 22:54:29 -0300

    feat: api/routes - añadido manejadores de rutas (#16)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,21 @@
 const { Router } = require('express');
-const getProducts = require('../handlers/getProducts');
-// Importar todos los routers;
 const router = Router();
+
+// Importar todos los routers
+const getProducts = require('../handlers/getProducts');
+const clientsRoute = require('../handlers/clientsRoute');
+const adminRoute = require('../handlers/adminRoute')
+
+
+
 // Configurar los routers
-
-
 router.get('/', getProducts);
+router.use('/clients', clientsRoute);
+router.use('/admin', adminRoute);
 
+router.use('*', function(res) {
+    res.status(404).send('Page not found');
+  });
 
 
 
