e78a8a2f73a756b0ee29afd6b516b2c2e5cb7db1
Michelle Diaz <michellelunaby@gmail.com>
2023-07-19 20:30:26 -0600

    fix:conflicto routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,13 +3,25 @@ const getProducts = require('../handlers/getProducts');
 const getCategories = require('../handlers/getCategories');
 // Importar todos los routers;
 const router = Router();
+
+// Importar todos los routers
+const getProducts = require('../handlers/getProducts');
+const clientsRoute = require('../handlers/clientsRoute');
+const adminRoute = require('../handlers/adminRoute')
+
+
+
 // Configurar los routers
 
 
-router.get('/products', getProducts);
 router.get('/products/categories', getCategories);
+router.get('/', getProducts);
+router.use('/clients', clientsRoute);
+router.use('/admin', adminRoute);
 
-
+router.use('*', function(res) {
+    res.status(404).send('Page not found');
+  });
 
 
 
