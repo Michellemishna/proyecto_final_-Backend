3dddf725a30e4a52c25d03d50735cc758a329ca1
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-07-21 21:37:22 -0300

    actualizar back

--- api/src/controllers/filtersSortsControllers.js
+++ api/src/controllers/filtersSortsControllers.js
@@ -1,59 +1,56 @@
 const { Product } = require("../db");
 
+const filtOrdenProd = async (obj) => {
+  try {
+    //console.log(obj);
+    const { category, price_min, price_max, sort_by, order } = obj;
+    let products = await Product.findAll();
+    console.log(products);
 
-const filtOrdenProd = async (obj) =>{
-    try {
-        //console.log(obj);
-        const { category, price_min, price_max, sort_by, order } = obj;
-        let products = await Product.findAll();
-        console.log(products);
+    if (category) {
+      products = products.filter((product) => product.category === category);
+    }
 
-        if (category) {
-            products = products.filter(
-              (product) => product.category === category
-            );
-          }
-      
-          // Filtrar por precio mínimo
-          if (price_min) {
-            products = products.filter(
-              (product) => product.price >= parseInt(price_min)
-            );
-          }
-      
-          // Filtrar por precio máximo
-          if (price_max) {
-            products = products.filter(
-              (product) => product.price <= parseInt(price_max)
-            );
-          }
-      
-          // Ordenar según el campo y orden proporcionado
-          if (sort_by && order) {
-            products.sort((a, b) => {
-              const fieldA = a[sort_by];
-              const fieldB = b[sort_by];
-              if (order === "asc") {
-                return fieldA - fieldB;
-              } else {
-                return fieldB - fieldA;
-              }
-            });
-          }
-      
-/*           products.length === 0
+    // Filtrar por precio mínimo
+    if (price_min) {
+      products = products.filter(
+        (product) => product.price >= parseInt(price_min)
+      );
+    }
+
+    // Filtrar por precio máximo
+    if (price_max) {
+      products = products.filter(
+        (product) => product.price <= parseInt(price_max)
+      );
+    }
+
+    // Ordenar según el campo y orden proporcionado
+    if (sort_by && order) {
+      products.sort((a, b) => {
+        const fieldA = a[sort_by];
+        const fieldB = b[sort_by];
+        if (order === "asc") {
+          return fieldA - fieldB;
+        } else {
+          return fieldB - fieldA;
+        }
+      });
+    }
+
+    /*           products.length === 0
           ? 'No hay resultados para los filtros aplicados'
           : products; */
-          if(products.length === 0) return 'No hay resultados para los filtros aplicados';
-          return products;
-    } catch (error) {
-        console.log("Hubo un error al aplicar los filtros y ordenamientos: " + error);
-    }
+    if (products.length === 0)
+      return "No hay resultados para los filtros aplicados";
+    return products;
+  } catch (error) {
+    console.log(
+      "Hubo un error al aplicar los filtros y ordenamientos: " + error
+    );
+  }
 };
 
-
-
-
 module.exports = {
-    filtOrdenProd,
-  };
+  filtOrdenProd,
+};
--- api/src/handlers/filterSortsRoute.js
+++ api/src/handlers/filterSortsRoute.js
@@ -1,25 +1,23 @@
 const { Router } = require("express");
 const router = Router();
-const { filtOrdenProd } = require('../controllers/filtersSortsControllers')
-
+const { filtOrdenProd } = require("../controllers/filtersSortsControllers");
 
 // GET:
 router.get("/selection", async (req, res) => {
-    try {    
-        const { category, price_min, price_max, sort_by, order } = req.query;
-        const products = await filtOrdenProd({
-            category: category, 
-            price_min: price_min, 
-            price_max: price_max, 
-            sort_by: sort_by, 
-            order: order, 
-        });
-        res.status(200).send(products)
-    } catch (error) {
-        res.status(400).send({ message: error.message });
-    }
+  try {
+    const { category, price_min, price_max, sort_by, order } = req.query;
+    console.log(price_max);
+    const products = await filtOrdenProd({
+      category: category,
+      price_min: price_min,
+      price_max: price_max,
+      sort_by: sort_by,
+      order: order,
+    });
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
 });
 
-
 module.exports = router;
-
